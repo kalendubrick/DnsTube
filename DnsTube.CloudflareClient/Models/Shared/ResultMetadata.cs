5bd2fc88af0cc43f08b658e0263d4b724ecf83ed
Kalen Dubrick <kalen@kalendubrick.com>
2020-10-24 07:18:57 -0500

    Add shared metadata class

---
+++ DnsTube.CloudflareClient/Models/Shared/ResultMetadata.cs
@@ -0,0 +1,28 @@
+﻿namespace DnsTube.CloudflareClient.Models.Shared
+{
+    /// <summary>
+    /// Contains paging information about response from the Cloudflare API.
+    /// </summary>
+    class ResultMetadata
+    {
+        /// <summary>
+        /// Gets or sets the result page number.
+        /// </summary>
+        public int Page { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of results per page.
+        /// </summary>
+        public int ResultsPerPage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the current result count.
+        /// </summary>
+        public int Count { get; set; }
+
+        /// <summary>
+        /// Gets or sets the total result count.
+        /// </summary>
+        public int TotalCount { get; set; }
+    }
+}
