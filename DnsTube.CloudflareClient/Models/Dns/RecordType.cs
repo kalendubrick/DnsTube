c1514629c351d8aac99265eb4ebb642ea0b8beea
Kalen Dubrick <kalen@kalendubrick.com>
2020-10-24 08:16:36 -0500

    Add RecordType string enum

---
+++ DnsTube.CloudflareClient/Models/Dns/RecordType.cs
@@ -0,0 +1,18 @@
+﻿namespace DnsTube.CloudflareClient.Models.Dns
+{
+    /// <summary>
+    /// String enum with relevant DNS record types.
+    /// </summary>
+    public sealed class RecordType
+    {
+        /// <summary>
+        /// Represents the IPv4 address record type.
+        /// </summary>
+        public static readonly string A = "A";
+
+        /// <summary>
+        /// Represents the IPv6 address record type.
+        /// </summary>
+        public static readonly string AAAA = "AAAA";
+    }
+}
