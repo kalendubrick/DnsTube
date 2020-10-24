07a5994e1e4829db81027e66768306bc62d9142b
Kalen Dubrick <kalen@kalendubrick.com>
2020-10-24 08:16:17 -0500

    Add Meta class

---
+++ DnsTube.CloudflareClient/Models/Dns/Meta.cs
@@ -0,0 +1,18 @@
+﻿namespace DnsTube.CloudflareClient.Models.Dns
+{
+    /// <summary>
+    /// Represents the meta field in the List DNS records response.
+    /// </summary>
+    public class Meta
+    {
+        /// <summary>
+        /// Gets or sets a bool indicating if the record was automatically added.
+        /// </summary>
+        public bool AutoAdded { get; set; }
+
+        /// <summary>
+        /// Gets or sets the source of the record.
+        /// </summary>
+        public string Source { get; set; }
+    }
+}
