64867f81372ff35313701a298f8bef6db7227cf4
Kalen Dubrick <kalen@kalendubrick.com>
2020-10-24 09:08:23 -0500

    Add class for DNS patch request

---
+++ DnsTube.CloudflareClient/Models/Dns/UpdateInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace DnsTube.CloudflareClient.Models.Dns
+{
+    /// <summary>
+    /// Represents the data to submit with a patch request on a DNS record.
+    /// </summary>
+    class UpdateInfo
+    {
+        /// <summary>
+        /// Gets or sets the updated IPv4 or IPv6 address.
+        /// </summary>
+        public string UpdatedAddress { get; set; }
+    }
+}
