0fd1ffee92b4fce8ad86b7f17af2c865d1e95be1
Kalen Dubrick <kalen@kalendubrick.com>
2020-11-08 15:32:35 -0600

    Add method to get zones

--- DnsTube.CloudflareClient/CloudflareClient.cs
+++ DnsTube.CloudflareClient/CloudflareClient.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Net.Http;
 
 using DnsTube.CloudflareClient.Models;
@@ -29,8 +29,56 @@ namespace DnsTube.CloudflareClient
             _emailAddress = emailAddress ?? throw new ArgumentNullException(nameof(emailAddress));
         }
 
+        /// <summary>
+        /// Gets a list of zones available.
+        /// </summary>
+        /// <returns>An <see cref="IEnumerable{Zone}"/> of available zones.</returns>
+        public async Task<IEnumerable<Zone>> GetZoneIdsAsync()
+        {
+            const string ZoneEndpoint = "zones?status=active&page=1&per_page=50&order=name&direction=asc&match=all";
+
+            using (var request = GetRequestMessage(HttpMethod.Get, ZoneEndpoint))
+            using (var response = await _client.SendAsync(request))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    var result = JsonConvert.DeserializeObject<ListSuccessResponse>(content);
+
+                    return result.Result as IEnumerable<Zone>;
+                }
+                else
+                {
+                    // TODO: Parse error status code and throw appropriate extension
+                    throw new Exception();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Creates an <see cref="HttpRequestMessage" /> for the specified method and uri.
+        /// Sets headers based on presence of token or api key/email address
+        /// </summary>
+        /// <param name="httpMethod">The <see cref="HttpMethod"/> to use for the request.</param>
+        /// <param name="requestUri">The uri to use for the request.</param>
+        /// <returns>An <see cref="HttpRequestMessage"/> for the specified method and uri.</returns>
         private HttpRequestMessage GetRequestMessage(HttpMethod httpMethod, string requestUri)
         {
+            if (httpMethod is null)
+            {
+                throw new ArgumentNullException(nameof(httpMethod));
+            }
+
+            if (requestUri is null)
+            {
+                throw new ArgumentNullException(nameof(requestUri));
+            }
+
+            if (string.IsNullOrWhiteSpace(requestUri))
+            {
+                throw new ArgumentException("Cannot be empty", nameof(requestUri));
+            }
+
             var req = new HttpRequestMessage(httpMethod, requestUri);
 
             if (!string.IsNullOrWhiteSpace(_token))
