2cec491fa9f3cbda689734481801ef51f452fd0e
Kalen Dubrick <kalen@kalendubrick.com>
2020-11-08 14:16:22 -0600

    Add .net core test project

---
+++ DnsTube.CloudflareClient.CoreTests/CloudflareClientTests.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Net.Http;
+using Xunit;
+
+namespace DnsTube.CloudflareClient.CoreTests
+{
+    public class CloudflareClientTests
+    {
+        [Fact]
+        public void NullHttpClientPassedToConstructorShouldThrowArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new CloudflareClient(null, "token"));
+        }
+
+        [Fact]
+        public void NullTokenPassedToConstructorShouldThrowArgumentNullException()
+        {
+            var httpClient = new HttpClient();
+
+            Assert.Throws<ArgumentNullException>(() => new CloudflareClient(httpClient, null));
+        }
+
+        [Fact]
+        public void NullApiKeyPassedToConstructorShouldThrowArgumentNullException()
+        {
+            var httpClient = new HttpClient();
+
+            Assert.Throws<ArgumentNullException>(() => new CloudflareClient(httpClient, null, "email@example.com"));
+        }
+
+        [Fact]
+        public void NullEmailAddressPassedToConstructorShouldThrowArgumentNullException()
+        {
+            var httpClient = new HttpClient();
+
+            Assert.Throws<ArgumentNullException>(() => new CloudflareClient(httpClient, "key-123456", null));
+        }
+    }
+}
