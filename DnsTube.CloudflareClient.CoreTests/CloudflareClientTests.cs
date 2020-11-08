a6e9734afbab470d99de4fd555f695042a7f2b07
Kalen Dubrick <kalen@kalendubrick.com>
2020-11-08 16:56:11 -0600

    Add test method for GetZoneIdsAsync

--- DnsTube.CloudflareClient.CoreTests/CloudflareClientTests.cs
+++ DnsTube.CloudflareClient.CoreTests/CloudflareClientTests.cs
@@ -1,5 +1,13 @@
+using DnsTube.CloudflareClient.Models.Zone;
+using Moq;
+using Moq.Protected;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace DnsTube.CloudflareClient.CoreTests
@@ -34,6 +42,32 @@ namespace DnsTube.CloudflareClient.CoreTests
             var httpClient = new HttpClient();
 
             Assert.Throws<ArgumentNullException>(() => new CloudflareClient(httpClient, "key-123456", null));
+        }
+
+        [Fact]
+        public async Task GetZoneIdsAsyncShouldReturnEnumerableOfZones()
+        {
+            var content = File.ReadAllText("getZoneIdsSuccessResponse.json");
+
+            // arrange
+            var mockMessageHandler = new Mock<HttpMessageHandler>();
+            mockMessageHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync",
+                                                    ItExpr.IsAny<HttpRequestMessage>(),
+                                                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent(content)
+                });
+
+            var cfClient = new CloudflareClient(new HttpClient(mockMessageHandler.Object), "token");
+
+            // act
+            var result = await cfClient.GetZoneIdsAsync();
+
+            // assert
+            Assert.IsAssignableFrom<IEnumerable<Zone>>(result);
         }
     }
 }
